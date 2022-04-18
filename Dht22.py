7ea9572351e708df4f5d6111d49207ace42cdade
Shazzy-C <kapricorne01>
2022-04-18 15:12:29 -0500

    Update 1.3 log data

--- Dht22.py
+++ Dht22.py
@@ -10,23 +10,3 @@ while True:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
     else:
         print("Failed to retrieve data from humidity sensor")
-
-
-import time
-
-import adafruit_dht
-import board
-
-dht = adafruit_dht.DHT22(board.D2)
-
-while True:
-    try:
-        temperature = dht.temperature
-        humidity = dht.humidity
-        # Print what we got to the REPL
-        print("Temp: {:.1f} *C \t Humidity: {}%".format(temperature, humidity))
-    except RuntimeError as e:
-        # Reading doesn't always work! Just print error and we'll try again
-        print("Reading from DHT failure: ", e.args)
-
-    time.sleep(1)
